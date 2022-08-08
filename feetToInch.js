5b632fb7a007d8e706f16a63b4a89f47fa274414
theMorshed <morshedfive@gmail.com>
2022-08-08 21:40:04 +0600

    wrote feetToInch function

---
+++ feetToInch.js
@@ -0,0 +1,11 @@
+/*
+১. সিম্পল একটা ফাংশন লিখতে হবে। যেটার নাম হবে feetToInch এবং এই ফাংশন ইনপুট হিসেবে নিবে feet আর রিটার্ন করবে inch । অর্থাৎ এই ফাংশনকে কোন একটা ফিট বলে দিলে সে রিটার্ন হিসেবে বলে দিবে কত ইঞ্চি হয়। 
+*/
+
+// declaration of a function
+function feetToInch(feet) {
+    return feet * 12;
+}
+
+console.log(feetToInch(3));
+console.log(feetToInch(4.67));
